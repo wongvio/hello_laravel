636c8d1d5d95dceea9fcb476ffbf9425df806c89
wongvio <45665078@qq.com>
2017-08-03 14:37:54 +0800

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5,Hello World!</div>
+                <div class="title">Laravel 5,哈哈，Hello World!</div>
             </div>
         </div>
     </body>
